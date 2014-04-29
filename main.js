3cbbcae8ba6cfaa6709de83079ece7b34d9a688d
Matthew Falbo <matthew.falbo@gmail.com>
2014-04-29 10:01:09 -0600

    Three pages, can click forward and backward through pages.  Can close entire book, and all pages by clicking open/close book button.

--- main.js
+++ main.js
@@ -22,6 +22,7 @@ $(document).on('ready', function() {
 		}else{
 			$(this).removeClass('cur');
 			$('#view-cover').addClass('cur');
+			$('.main').find('.book-page.page-flip').removeClass('page-flip');
 			book.removeClass().addClass('view-cover');
 		}
 	});
@@ -38,25 +39,35 @@ $(document).on('ready', function() {
 		}
 	});
 
-	// Close book by clicking inside of front cover
+	// Go to previous page, or close book by clicking inside of front cover
+	// Not ideal, but functional for now
 	$(document).on('click', '.book-cover-back', function(){
-		if ( book.attr('class') != 'open-book') {
-			$('#open-book').addClass('cur').siblings().removeClass('cur');
-			book.removeClass().addClass('open-book');
-		}else{
-			$('#open-book').removeClass('cur');
-			$('#view-cover').addClass('cur');
-			book.removeClass().addClass('view-cover');
-		}
+
+			if(($('.main').find('.book-page.page-flip')).length === 0){
+				if ( book.attr('class') != 'open-book') {
+					$('#open-book').addClass('cur').siblings().removeClass('cur');
+					book.removeClass().addClass('open-book');
+				}
+				else{
+					$('#open-book').removeClass('cur');
+					$('#view-cover').addClass('cur');
+					book.removeClass().addClass('view-cover');
+				}
+			}
+
+			$('.main').find('.book-page.page-flip:first').removeClass('page-flip')
+			.next().children('.book-page-back').css('visibility', 'visible');
 	});
 
-	// Flip page on when clicked on
+	// Flip page when clicked on
 	$(document).on('click', '.book-page', function(){
 		
 		$(this).addClass('page-flip');
-		// $('.book-page-back').addClass('page-flip');
+		$(this).find('.book-page-back').addClass('visible-back-page').css('visibility', 'visible');
+		$(this).siblings().find('.book-page-back').css('visibility', 'hidden');
 	
 	});
+
 
 // Rotate book 360 degrees when button is clicked
 $('#view-rotate').click(function(){
